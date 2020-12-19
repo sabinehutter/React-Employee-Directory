e048357550eca1820236010278077cef5ec5a32b
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-19 02:31:07 -0500

    adding handleinputchange and api call

--- enployee-directory/src/App.js
+++ enployee-directory/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import API from "./utils/API"
+import Card from "react-bootstrap/Card";
+
 // import Main from "./components/Main";
 
 function FormInput (){
   return <div>form input</div>
 }
 
-function UsersView (props){
+function UsersView (){
   // props: user (filteredUsers)
   // button to filter by name
     // onClick -> this.sort
-  return <div>users view</div>
+    return (
+      <div>
+        <h1 className="text-center">Make New Friends</h1>
+        <h3 className="text-center">
+          Thumbs up on any pups you'd like to meet!
+        </h3>
+        <Card image={this.state.name} />
+      </div>
+    );
 }
 
 class App extends React.Component{
@@ -22,14 +33,42 @@ state = {
 }
 
 // component did mount
-  // call API (utils)
-    // set state
+
+componentDidMount() {
+  this.getUsers()
+  .catch(err => console.log(err));
+}
+
+getUsers = () => {
+    // call API (utils)
+  API.getUsers()
+    .then(res => 
+      // console.log(res.data.picture.thumbnail)
+          // set state
+      this.setState({
+        allUsers : {
+        ...this.state.allUsers,
+        image : res.data.picture.thumbnail,
+        name : res.data.name.first + res.data.name.last,
+        phone: res.data.phone,
+        email: res.data.email,
+        dob: res.data.dob.date}
+
+      })
+    )
+    .catch(err => console.log(err));
+};
       // set up allUsers : [], filteredUsers: []
 
   // handleInputChange
+  handleInputChange = event => {
     // change (setState) searchTerm
+
+    this.setState({ searchTerm: event.target.value });
+
     // filter users on searchTerm
     // sort iif relevant
+  };
 
    // sortUsers
     // using js sort (by string. or if youo want to sort by number (DOB))
