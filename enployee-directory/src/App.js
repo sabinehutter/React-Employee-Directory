a5e10a2d08419bd83db4c2d7425fa5335403f946
Sabine-Hutter <Sabine.Hutter@ibm.com>
2020-12-20 14:08:48 -0500

    adding sorting

--- enployee-directory/src/App.js
+++ enployee-directory/src/App.js
@@ -73,24 +73,63 @@ componentDidMount() {
             console.log(res)
             // set state
             this.setState({
-              allUsers: res.data.results
+              allUsers: res.data.results,
+              filteredUsers: res.data.results
             })
           })
       };
             // set up allUsers : [], filteredUsers: []
       
         // handleInputChange
-    handleSearchChange = event => {
+    handleSearchChange = (event) => {
         // change (setState) searchTerm
-        console.log(event.target.value)
-        this.setState({ searchTerm: event.target.value });
+        console.log(event.target)
+        const {name, value } = event.target;
+
+        this.setState(
+          {
+            [name]: {value},
+          },
+          () => {
+              const filtered = this.state.allUsers.filter((result) =>
+                // console.log(result)
+                result.name.first.includes(this.state.searchName)
+              );
+              console.log(filtered);
+              this.setState({
+                filteredUsers: filtered,
+              });
+            }
+          );
+        };
   
       // filter users on searchTerm
         // use javascript function
       // sort if relevant (nice to have)
-    };
   
      // sortUsers
+     handleSubmit = (event) => {
+      // Preventing the default behavior of the form submit (which is to refresh the page)
+      event.preventDefault();
+      if (this.state.trackSort !== "ascend") {
+        const sorted = this.state.allUsers.sort((a, b) => a.name.first.localeCompare(b.name.first))
+        console.log(sorted)
+        console.log("clicked")
+        this.setState({
+          filteredUsers: sorted,
+          trackSort:"ascend"
+        });
+      }
+  
+      else{  const sorted = this.state.allUsers.sort((a, b) => b.name.first.localeCompare(a.name.first))
+        console.log(sorted)
+        console.log("clicked")
+        this.setState({
+          filteredUsers: sorted,
+          trackSort:"descend"
+        });}
+  
+    };
       // using js sort (by string. or if youo want to sort by number (DOB))
       // optionoal implement, up or down` as
   
@@ -100,17 +139,22 @@ render(){
         <Header />
         <nav class="navbar navbar-light bg-light mx-auto" style={{height : "100px"}}>
         <form class="form-inline">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" onChange = {this.handleSearchChange} value = {this.searchTerm}></input>
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" onChange = {this.handleSearchChange} value = {this.state.searchTerm}></input>
         </form>
       </nav>
-        {/* <SearchBar /> */}
+      <div className="row">
+            <div className="table-title col">Image</div>
+            <div className="table-title click col-3" onClick={this.handleSubmit}>Name</div>
+            <div className="table-title col">Phone Number</div>
+            <div className="table-title col">Email</div>
+            <div className="table-title col">DOB</div>
+          </div>
         {/* <FormInput /> */}
         {this.state.allUsers.map((user, index) => (
           // console.log(user)
           <UsersView user={user} key={index} />
         )
         )}
-        {/* <UsersView users = {this.state.allUsers}/> */}
       </div>
       );
 }
